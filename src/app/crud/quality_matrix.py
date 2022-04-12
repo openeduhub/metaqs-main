6c20d6c51f3e8e4ec6e20cdc55fbb9acaf1eeec8
Robert Meissner <robert.meissner@codecentric.de>
2022-04-12 15:56:34 +0200

    WIP: Quality matrix exploration
    WIP

--- src/app/crud/quality_matrix.py
+++ src/app/crud/quality_matrix.py
@@ -3,6 +3,7 @@ from typing import List, Dict
 from elasticsearch_dsl import AttrDict
 from elasticsearch_dsl.response import Response
 
+from app.core.config import ELASTIC_MAX_SIZE
 from app.core.logging import logger
 from app.crud.elastic import base_filter
 from app.elastic import Search, qbool, qexists
@@ -16,12 +17,10 @@ def extract_replication_source(data: List[AttrDict]) -> Dict:
     # TODO: Rewrite functional
     for attribute_element in data:
         element = attribute_element.to_dict()
-        logger.debug(f"Evaluating element: {element}")
-        logger.debug(f"Evaluating element, properties: {element[PROPERTIES]}")
+        #logger.debug(f"Evaluating element: {element}")
 
         if REPLICATION_SOURCE in element[PROPERTIES].keys():
             replication_source = element[PROPERTIES][REPLICATION_SOURCE]
-            print(replication_source)
             if replication_source not in result.keys():
                 result.update({replication_source: {"entries": 0}})
             result[replication_source]["entries"] += 1
@@ -35,8 +34,7 @@ def extract_replication_source(data: List[AttrDict]) -> Dict:
                     result[replication_source].update({key: content_value})
                 else:
                     result[replication_source][key] = content_value + result[replication_source][key]
-                print(content, content_value, result[replication_source][key])
-    print(result)
+                #logger.debug(content, content_value, result[replication_source][key])
     return result
     # TODO: Potentially some sources do not have all properties
 
@@ -45,7 +43,7 @@ async def get_quality_matrix():
     qfilter = [*base_filter]
     s = Search().query(qbool(filter=qfilter))
 
-    response: Response = s.source(includes=[f'{PROPERTIES}.*'], excludes=[])[:100].execute()
+    response: Response = s.source(includes=[f'{PROPERTIES}.*'], excludes=[])[:ELASTIC_MAX_SIZE].execute()
 
     if response.success():
         return extract_replication_source(response.hits)
