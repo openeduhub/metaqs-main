46dbe2ebd0c7d9760b3654c00e1d9ec0b99d3899
Robert Meissner <robert.meissner@codecentric.de>
2022-04-19 15:03:38 +0200

    WIP: Quality matrix exploration
    WIP

--- src/app/crud/quality_matrix.py
+++ src/app/crud/quality_matrix.py
@@ -27,7 +27,7 @@ def write_to_json(filename: str, response):
         json.dump([hit.to_dict() for hit in response.hits], outfile)
 
 
-def get_sources() -> list:
+def get_sources() -> list[str, int]:
     aggregation_name = "unique_sources"
     main_query = {
         "query": {
@@ -55,7 +55,7 @@ def get_sources() -> list:
     s = Search().from_dict(main_query)
     s.aggs.bucket(aggregation_name, "terms", field="properties.ccm:replicationsource.keyword")
     response: Response = s.execute()
-    return [entry["key"] for entry in response.aggregations.to_dict()[aggregation_name]["buckets"]]
+    return {entry["key"]:entry["doc_count"] for entry in response.aggregations.to_dict()[aggregation_name]["buckets"]}
 
 
 def extract_properties(hits: list[AttrDict]) -> list:
@@ -97,7 +97,7 @@ def get_properties():
 async def get_quality_matrix():
     output = {}
 
-    for source in get_sources():
+    for source, total_count in get_sources():
         output.update({source: {}})
         for field in get_properties():
             non_empty_entries = {
@@ -138,38 +138,6 @@ async def get_quality_matrix():
             s = Search().from_dict(non_empty_entries)
             print(f"From dict counting: {s.to_dict()}")
             count: int = s.source().count()
-
-            all_entries = {
-                "query": {
-                    "bool": {
-                        "must": [
-                            {
-                                "match": {
-                                    "properties.ccm:replicationsource": source
-                                }
-                            },
-                            {
-                                "match": {
-                                    "permissions.Read": "GROUP_EVERYONE"
-                                }
-                            },
-                            {
-                                "match": {
-                                    "properties.cm:edu_metadataset": "mds_oeh"
-                                }
-                            },
-                            {
-                                "match": {
-                                    "nodeRef.storeRef.protocol": "workspace"
-                                }
-                            }
-                        ],
-                    }
-                }
-            }
-            s = Search().from_dict(all_entries)
-            print(f"From dict all_entries: {s.to_dict()}")
-            total_count: int = s.source().count()
 
             empty_entries = {
                 "query": {
