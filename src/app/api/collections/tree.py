f8f109b854b98da4679b31f5d64c562698fa0725
Robert Meissner <robert.meissner@codecentric.de>
2022-06-10 13:43:13 +0200

    Updating API endpoints
    Loading collection tree from elastic search

--- src/app/api/collections/tree.py
+++ src/app/api/collections/tree.py
@@ -2,12 +2,21 @@ from __future__ import (  # Needed for recursive type annotation, can be dropped
     annotations,
 )
 
+from itertools import chain
 from uuid import UUID
 
 from aiohttp import ClientSession
+from elasticsearch_dsl.response import Response
+from glom import Coalesce, Iter, glom
 from pydantic import BaseModel
 
+from app.api.quality_matrix.quality_matrix import add_base_match_filters
+from app.core.config import ELASTIC_TOTAL_SIZE
 from app.core.constants import COLLECTION_ROOT_ID
+from app.elastic.dsl import qbool, qterm
+from app.elastic.fields import Field, FieldType
+from app.elastic.search import Search
+from app.models import ElasticResourceAttribute
 
 
 class CollectionTreeNode(BaseModel):
@@ -29,11 +38,31 @@ def collection_to_model(data: list[dict]) -> list[CollectionTreeNode]:
     ]
 
 
+def build_portal_tree(
+    collections: list, root_noderef_id: UUID
+) -> list[CollectionTreeNode]:
+    lut = {str(root_noderef_id): []}
+
+    for collection in collections:
+        if collection.title:
+            portal_node = CollectionTreeNode(
+                noderef_id=collection.noderef_id,
+                title=collection.title,
+                children=[],
+            )
+
+            try:
+                lut[str(collection.parent_id)].append(portal_node)
+            except KeyError:
+                lut[str(collection.parent_id)] = [portal_node]
+
+            lut[str(collection.noderef_id)] = portal_node.children
+
+    return lut.get(str(root_noderef_id), [])
+
+
 async def parsed_tree(session: ClientSession, node_id: UUID):
-    oeh_topic_route = (
-        "oeh-topics" if str(node_id) == COLLECTION_ROOT_ID else "oehTopics"
-    )
-    url = f"https://vocabs.openeduhub.de/w3id.org/openeduhub/vocabs/{oeh_topic_route}/{node_id}.json"
+    url = f"https://vocabs.openeduhub.de/w3id.org/openeduhub/vocabs/oehTopics/{node_id}.json"
     response = await session.get(url=url)
     if response.status == 200:
         data = await response.json()
@@ -41,7 +70,82 @@ async def parsed_tree(session: ClientSession, node_id: UUID):
         return collection_to_model(data[keyword])
 
 
+def tree_query(node_id: UUID) -> Search:
+    s = add_base_match_filters(
+        Search().query(qbool(filter=qterm(qfield="path", value=node_id)))
+    )
+    s = s.source(
+        ["nodeRef.id", "properties.cm:title", "collections.path", "parentRef.id"]
+    ).sort("fullpath")[:ELASTIC_TOTAL_SIZE]
+    return s
+
+
+class Collection(BaseModel):
+    noderef_id: UUID
+    title: str
+    children: list[CollectionTreeNode]
+    parent_id: UUID
+
+
+class _CollectionAttribute(Field):
+    TITLE = ("properties.cm:title", FieldType.TEXT)
+    DESCRIPTION = ("properties.cm:description", FieldType.TEXT)
+    PATH = ("path", FieldType.KEYWORD)
+    PARENT_ID = ("parentRef.id", FieldType.KEYWORD)
+    NODE_ID = ("nodeRef.id", FieldType.KEYWORD)
+
+
+CollectionAttribute = Field(
+    "CollectionAttribute",
+    [
+        (f.name, (f.value, f.field_type))
+        for f in chain(ElasticResourceAttribute, _CollectionAttribute)
+    ],
+)
+
+
+def hits_to_collection(hits: list) -> list[Collection]:
+    collections = []
+    for hit in hits:
+        entry = hit.to_dict()
+        spec = {
+            "title": Coalesce(CollectionAttribute.TITLE.path, default=None),
+            "keywords": (
+                Coalesce(CollectionAttribute.KEYWORDS.path, default=[]),
+                Iter().all(),
+            ),
+            "description": Coalesce(CollectionAttribute.DESCRIPTION.path, default=None),
+            "path": (
+                Coalesce(CollectionAttribute.PATH.path, default=[]),
+                Iter().all(),
+            ),
+            "parent_id": Coalesce(CollectionAttribute.PARENT_ID.path, default=None),
+            "node_id": Coalesce(CollectionAttribute.NODE_ID.path, default=None),
+        }
+        parsed_entry = glom(entry, spec)
+        if parsed_entry["title"] is not None:
+            collections.append(
+                Collection(
+                    noderef_id=parsed_entry["node_id"],
+                    title=parsed_entry["title"],
+                    children=[],
+                    parent_id=parsed_entry["parent_id"],
+                )
+            )
+    return collections
+
+
+def parse_tree(node_id: UUID):
+    response: Response = tree_query(node_id).execute()
+
+    if response.success():
+        collection = hits_to_collection(response)
+        return build_portal_tree(collection, node_id)
+
+
 # TODO: Include parent/current node
-async def collection_tree(node_id: UUID):
-    async with ClientSession() as session:
-        return await parsed_tree(session, node_id)
+async def collection_tree(node_id: UUID, use_vocabs: bool = False):
+    if use_vocabs:
+        async with ClientSession() as session:
+            return await parsed_tree(session, node_id)
+    return parse_tree(node_id)
--- tests/unit_tests/test_tree.py
+++ tests/unit_tests/test_tree.py
@@ -1,11 +1,18 @@
 import asyncio
+import json
 import uuid
 from unittest import mock
 from unittest.mock import MagicMock
 
 import pytest
+from elasticsearch_dsl.response import Response
 
-from app.api.collections.tree import collection_tree, parsed_tree
+from app.api.collections.tree import (
+    collection_tree,
+    parse_tree,
+    parsed_tree,
+    tree_query,
+)
 
 
 def node_count(data: list):
@@ -18,10 +25,10 @@ def node_count(data: list):
 @pytest.mark.asyncio
 async def test_collection_tree():
     root_node_id = uuid.UUID("5e40e372-735c-4b17-bbf7-e827a5702b57")
-    data = await collection_tree(root_node_id)
+    data = await collection_tree(root_node_id, use_vocabs=True)
     assert len(data) == 26
     count = node_count(data)
-    assert count == 2219
+    assert count == 2216
 
 
 @pytest.mark.asyncio
@@ -41,3 +48,73 @@ async def test_parsed_tree_empty_json():
 
         data = await parsed_tree(mocked_client, root_node_id)
         assert len(data) == 0
+
+
+def test_parse_tree():
+    node_id_biology = uuid.UUID("15fce411-54d9-467f-8f35-61ea374a298d")
+
+    query = tree_query(node_id_biology)
+    expected_query = {
+        "_source": [
+            "nodeRef.id",
+            "properties.cm:title",
+            "collections.path",
+            "parentRef.id",
+        ],
+        "from": 0,
+        "query": {
+            "bool": {
+                "filter": [{"term": {"path": node_id_biology}}],
+                "must": [
+                    {"match": {"permissions.Read": "GROUP_EVERYONE"}},
+                    {"match": {"properties.cm:edu_metadataset": "mds_oeh"}},
+                    {"match": {"nodeRef.storeRef.protocol": "workspace"}},
+                ],
+            }
+        },
+        "size": 500000,
+        "sort": ["fullpath"],
+    }
+
+    assert query.to_dict() == expected_query
+
+    with open("unit_tests/resources/test_tree.json") as file:
+        expected_tree = json.loads("".join(file.readlines()))
+    with open("unit_tests/resources/test_tree_response.json") as file:
+        response = json.loads("".join(file.readlines()))
+        response = [hit["_source"] for hit in response]
+
+    with mock.patch("app.api.collections.tree.Search.execute") as mocked_execute:
+        dummy_hits = {"hits": {"hits": response}}
+        mocked_response = Response(
+            search=query,
+            response={"test": "hello", "aggregations": {}, "hits": dummy_hits},
+        )
+        mocked_response._d_ = dummy_hits
+        mocked_response._hits = response
+        mocked_response.success = MagicMock()
+        mocked_response.success.return_value = True
+        mocked_execute.return_value = mocked_response
+        tree = parse_tree(node_id_biology)
+
+    def tree_to_json(_tree: dict) -> list:
+        return [
+            {
+                "noderef_id": collection.noderef_id,
+                "title": collection.title,
+                "children": tree_to_json(collection.children)
+                if collection.children
+                else [],
+            }
+            for collection in _tree
+        ]
+
+    json_tree = tree_to_json(tree)
+
+    assert len(json_tree) == len(expected_tree)
+
+    def top_nodes(data: list) -> set:
+        return {str(entry["noderef_id"]) for entry in data}
+
+    assert top_nodes(json_tree) == top_nodes(expected_tree)
+    assert json_tree == expected_tree
