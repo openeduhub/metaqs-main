658265e7cbe3ead3ac39fc3a4808b73e650f522f
Robert Meissner <robert.meissner@codecentric.de>
2022-08-04 09:21:21 +0200

    #72 Writing timeline to DB
    Cleanup
    
    KBMBF-510
    KBMBF-452
    SD_WLO-238

--- src/app/api/quality_matrix/timeline.py
+++ src/app/api/quality_matrix/timeline.py
@@ -8,13 +8,14 @@ from app.api.quality_matrix.collections import collection_quality
 from app.api.quality_matrix.models import Forms, Timeline
 from app.api.quality_matrix.replication_source import source_quality
 from app.core.constants import COLLECTION_NAME_TO_ID
+from app.core.logging import logger
 from app.db.tasks import store_in_timeline
 
 
 async def quality_backup(database: Database):
     for form in [Forms.REPLICATION_SOURCE, Forms.COLLECTIONS]:
         for node_id in COLLECTION_NAME_TO_ID.values():
-            print(node_id)
+            logger.info(f"Backing up for node id: '{node_id}'")
             if form == Forms.REPLICATION_SOURCE:
                 quality_data, total = await source_quality(uuid.UUID(node_id))
             else:  # Forms.COLLECTIONS:
