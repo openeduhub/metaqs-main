a3d5e7fdd7a8e09a29c1270dc3b827de4a2daa0e
Martin Rueckl <martin.rueckl@codecentric.de>
2022-08-16 17:42:49 +0200

    improve database logging and create table on application startup

--- src/app/db/tasks.py
+++ src/app/db/tasks.py
@@ -11,20 +11,31 @@ from app.db.core import database_url
 
 
 async def connect_to_db(app: FastAPI) -> None:
+    logger.info("Instantiating database")
     database = Database(database_url(), min_size=2, max_size=10)
 
     try:
         await database.connect()
+        await database.execute('''
+        create table if not exists timeline
+        (
+            id        int,
+            timestamp int,
+            mode      text,
+            quality   json,
+            total     json,
+            node_id   uuid
+        );''')
         app.state._db = database
-    except Exception:
-        logger.exception("")
+    except Exception as e:
+        logger.exception(f"Failed to connect to database and create table: {e}")
 
 
 async def close_db_connection(app: FastAPI) -> None:
     try:
         await app.state._db.disconnect()
-    except Exception:
-        logger.exception("")
+    except Exception as e:
+        logger.exception(f"Failed to disconnect from database: {e}")
 
 
 async def store_in_timeline(
